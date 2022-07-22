9243b3b0002dfd8af3f02381576bfa884a3c2791
Christian Torrico Avila <chtorrico@ecisolutions.com>
2022-07-21 20:52:10 -0400

    swipeable card: moved files to view package

--- app/src/main/java/com/locotoDevTeam/diccionariocamba/view/MainActivity.kt
+++ app/src/main/java/com/locotoDevTeam/diccionariocamba/view/MainActivity.kt
@@ -1,14 +1,11 @@
-package com.locotoDevTeam.diccionariocamba
+package com.locotoDevTeam.diccionariocamba.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.locotoDevTeam.diccionariocamba.R
 import com.locotoDevTeam.diccionariocamba.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
---
+++ app/src/main/java/com/locotoDevTeam/diccionariocamba/view/SlideFragment.kt
@@ -0,0 +1,20 @@
+package com.locotoDevTeam.diccionariocamba.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.locotoDevTeam.diccionariocamba.R
+
+
+class SlideFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_slide, container, false)
+    }
+}
