c07e98d58ff309cfe796d78c4afc03d40fb73851
Christian Torrico Avila <chtorrico@ecisolutions.com>
2022-07-21 23:53:00 -0400

    swipeable card: added images and adapter

--- app/src/main/java/com/locotoDevTeam/diccionariocamba/view/SlideFragment.kt
+++ app/src/main/java/com/locotoDevTeam/diccionariocamba/view/SlideFragment.kt
@@ -5,16 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
 import com.locotoDevTeam.diccionariocamba.R
+import com.locotoDevTeam.diccionariocamba.adapter.ItemKolodaAdapter
+import com.locotoDevTeam.diccionariocamba.databinding.FragmentSlideBinding
+import com.locotoDevTeam.diccionariocamba.model.DataSource
+import com.yalantis.library.Koloda
 
 
 class SlideFragment : Fragment() {
 
+    lateinit var sliderBinding: FragmentSlideBinding
+    val dataSource = DataSource().loadImages()
+    lateinit var recyclerView: RecyclerView
+    lateinit var koloda: Koloda
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_slide, container, false)
+    ): View {
+        val view =  inflater.inflate(R.layout.fragment_slide, container, false)
+        sliderBinding = FragmentSlideBinding.bind(view)
+        return sliderBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    private fun initRecycler(){
+        val adapter = ItemKolodaAdapter(requireContext(), dataSource)
+        koloda.adapter = adapter
     }
 }
